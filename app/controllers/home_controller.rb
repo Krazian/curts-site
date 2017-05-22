0cec2fe4dfedacc57de1276176d28b80f536cba7
Johnathan Chei <j.chei175@gmail.com>
2017-05-21 22:39:25 -0400

    landing page and part of about page

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,30 @@
+class HomeController < ApplicationController
+
+def splash
+end
+
+def video
+
+end
+
+def portfolio
+
+end
+
+def graphics
+
+end
+
+def projects
+
+end
+
+def about
+
+end
+
+def contact
+
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "home#splash"
+  get "videogallery", to: "home#video"
+  get "portfolio", to: "home#portfolio"
+  get "graphic-gallery", to: "home#graphics"
+  get "projects", to: "home#projects"
+  get "about", to: "home#about"
+  get "contact", to: "home#contact"
 end
